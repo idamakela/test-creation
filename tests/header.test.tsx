8fc5bccad126a6756991ffefccd3abaa01fdfc92
ell-ska <ella@skagerstrom.se>
2023-11-23 10:53:27 +0100

    add randomizer button tests to header

--- tests/header.test.tsx
+++ tests/header.test.tsx
@@ -1,20 +1,45 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Header from '@/components/Header'
 
+const mockProps = {
+  randomizeColors: jest.fn()
+}
+
 describe('Header', () => {
-  test('is semantic', () => {
-    render(<Header />)
+  describe('is rendered correctly', () => {
+    test('it is semantic', () => {
+      render(<Header {...mockProps} />)
+  
+      const headerElement = screen.getByRole('banner')
+  
+      expect(headerElement).toBeInTheDocument()
+    })
+  
+    test('it has a heading', () => {
+      render(<Header {...mockProps} />)
+  
+      const heading = screen.getByRole('heading', { level: 1 })
+  
+      expect(heading).toBeInTheDocument()
+    })
+  
+    test('it has a randomizer button', () => {
+      render(<Header {...mockProps} />)
+  
+      const randomizerButton = screen.getByTitle(/randomize colors/i)  
 
-    const headerElement = screen.getByRole('banner')
-
-    expect(headerElement).toBeInTheDocument()
+      expect(randomizerButton).toBeInTheDocument()
+    })
   })
 
-  test('has a heading', () => {
-    render(<Header />)
-
-    const heading = screen.getByRole('heading', { level: 1 })
-
-    expect(heading).toBeInTheDocument()
+  describe('functions as expected', () => {
+    test('the randomizer function is called on click', () => {
+      render(<Header {...mockProps} />)
+  
+      const randomizerButton = screen.getByTitle(/randomize colors/i)
+      fireEvent.click(randomizerButton)
+  
+      expect(mockProps.randomizeColors).toHaveBeenCalled()
+    })
   })
 })
