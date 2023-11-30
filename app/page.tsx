14f47e826987bab29305d439448520862a9d1033
idamakela <idamakela98@gmail.com>
2023-11-30 15:36:27 +0100

    fetch hex color name, move header to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
+'use client'
+
+import Header from '@/components/Header'
+
+import { fetchData } from '@/lib/fetchData'
+import { useData } from '@/hooks/useData'
+import { useEffect } from 'react'
+
+const randomizeColors = async () => {
+  // get random colors
+}
+
 export default function Home() {
+  const data = useData('a3e2dd')
+
+  useEffect(() => {
+    console.log(data)
+  })
+
   return (
-    <main>main</main>
+    <>
+      <Header randomizeColors={randomizeColors} />
+      <main>main</main>
+    </>
   )
 }
